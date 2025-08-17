7528999ce0e1b66d9674092e345b183dfadeaebc
Mugabo Andre <andremugabo@yahoo.fr>
2025-08-17 23:02:58 +0200

    first commit

---
+++ rentalhub-backend/src/main/java/rw/mugaboandre/rentalhub/RentalhubBackendApplication.java
@@ -0,0 +1,13 @@
+package rw.mugaboandre.rentalhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalhubBackendApplication.class, args);
+	}
+
+}
---
+++ rentalhub-backend/src/test/java/rw/mugaboandre/rentalhub/RentalhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.mugaboandre.rentalhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
